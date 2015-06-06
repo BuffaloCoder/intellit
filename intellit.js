978e6b40e035758d262184bae13ecabf31cea85c
joe <joesullivan64@gmail.com>
2015-06-06 17:57:23 -0400

    github api, basic issue pulling

--- intellit.js
+++ intellit.js
@@ -1,6 +1,8 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  // no issues loaded
+  Session.setDefault('issues', '');
 
   Template.hello.helpers({
     counter: function () {
@@ -12,6 +14,31 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+  Template.git.helpers({
+    issues: function () {
+      return Session.get('issues');
+    }
+  });
+
+  Template.git.events({
+    'click .all-issues': function () {
+      HTTP.call("GET", "https://api.github.com/repos/mikedeboer/node-github/issues",
+        function (error, result) {
+          if (error) {
+            console.log(error.message);
+          } else {
+            var issues = result.data;
+            for (var i = 0; i < issues.length; i++) {
+              var message = Session.get('issues');
+              var issue = issues[i].body;
+              Session.set('issues', message + issue + '<br>');
+              console.log(issue);
+            };
+          }
+      });
     }
   });
 }
