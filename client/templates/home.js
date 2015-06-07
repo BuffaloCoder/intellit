5e6876378e01dec97ab5ab32c4ca3af0a8a0912f
joe <joesullivan64@gmail.com>
2015-06-06 20:46:49 -0400

    Uses github login token and updated markdown display

--- client/templates/home.js
+++ client/templates/home.js
@@ -11,17 +11,25 @@ Template.home.helpers({
 
 Template.home.events({
   'click .all-issues': function () {
-    HTTP.call("GET", "https://api.github.com/repos/mikedeboer/node-github/issues",
+    var token = Meteor.user().services.github.accessToken;
+    console.log(token);
+    var url = "https://api.github.com/repos/mikedeboer/node-github/issues";
+    var options = {
+      params: {
+        "access_token": token
+      }
+    };
+    HTTP.call("GET", url, options,
       function (error, result) {
         if (error) {
           console.log(error.message);
         } else {
           var issues = result.data;
           for (var i = 0; i < issues.length; i++) {
-            var message = Session.get('issues');
+            var title = issues[i].title;
             var issue = issues[i].body;
-            Session.set('issues', message + issue + '<br>');
-            console.log(issue);
+            var message = "_::" + title + "::_\n" + issue + "\n\n";
+            Session.set('issues', Session.get('issues') + message);
           };
         }
     });
