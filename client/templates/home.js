9a4fcca3b3a09c34747bd7e77922936ad8cc3d73
joe <joesullivan64@gmail.com>
2015-06-07 09:56:43 -0400

    Implemented form to add issues

--- client/templates/home.js
+++ client/templates/home.js
@@ -17,8 +17,8 @@ function getIssues (url, options) {
     });
 }
 
-function createIssue(user, repo, options) {
-  var url = "https://api.github.com/repos/" + user + "/" + repo + "/issues";
+function createIssue(options) {
+  var url = Session.get('repo_url') + "/issues";
   HTTP.post(url, options,
       function (error, res) {
         if (error) {
@@ -29,7 +29,17 @@ function createIssue(user, repo, options) {
     });
 }
 
-function getRepos(options) {
+function getRepos() {
+  try {
+    var token = Meteor.user().services.github.accessToken;
+  } catch (error) { // not logged in yet
+    return;
+  }
+  var options = {
+    params: {
+      "access_token": token
+    }
+  };
   var url = "https://api.github.com/user/repos";
   var repos = {};
   HTTP.get(url, options,
@@ -46,6 +56,30 @@ function getRepos(options) {
           Session.set('repos', repos);
         }
     });
+  return Session.get('repos');
+}
+
+function getAssignees() {
+  var token = Meteor.user().services.github.accessToken;
+  var options = {
+    params: {
+      "access_token": token
+    }
+  };
+  var url = Session.get('repo_url') + "/assignees";
+  HTTP.get(url, options,
+      function (error, res) {
+        if (error) {
+          console.log(error.message);
+        } else {
+          var assignees = [];
+          for (var i = 0; i < res.data.length; i++) {
+            var name = res.data[i].login;
+            assignees.push({name: name});
+          }
+          Session.set('assignees', assignees);
+        }
+    });
 }
 
 Template.home.rendered = function(){
@@ -53,6 +87,7 @@ Template.home.rendered = function(){
   Session.setDefault('issues', '');
   Session.setDefault('repos', []);
   Session.setDefault('repo_url', '');
+  Session.setDefault('assignees', []);
 };
 
 Template.home.helpers({
@@ -60,33 +95,25 @@ Template.home.helpers({
     return Session.get('issues');
   },
   repo: function () {
-    console.log(Session.get('repos'));
-    return Session.get('repos');
+    return getRepos();
+    // return Session.get('repos');
+  },
+  assignee: function () {
+    return Session.get('assignees');
   },
 });
 
 Template.home.events({
-  'click .all-issues': function () {
+  'submit .create-issue': function (event) {
+    var title = event.target.title.value;
+    var body = event.target.body.value;
+    var assigneeElement = event.target.assigneeSelect;
+    var assignee = assigneeElement.options[assigneeElement.selectedIndex].text;
+    var milestone = event.target.milestone.value;
+    // need to parse into array or something
+    var labels = event.target.labels.value;
+
     var token = Meteor.user().services.github.accessToken;
-    var options = {
-      params: {
-        "access_token": token
-      }
-    };
-    // var url = "https://api.github.com/repos/mikedeboer/node-github/issues";
-    getIssues(Session.get("repo_url") + "/issues", options);
-  },
-  'click .create-issue': function () {
-    console.log(Meteor.user());
-    // these should be parameters
-    var title = "Found another bug";
-    var body = "I'm having a problem with this.";
-    var assignee = "dwrvnmnr";
-    var milestone = 1;
-    var labels = ["Label1", "Label2"];
-    //
-    var token = Meteor.user().services.github.accessToken;
-    var username = Meteor.user().services.github.username;
     var options = {
       data: {
         "title": title,
@@ -99,22 +126,27 @@ Template.home.events({
         "access_token": token
       }
     };
-    var user = "dwrvnmnr";
-    var repo = "intellit";
-    createIssue(user, repo, options);
+    createIssue(options);
+
+    // clear form
+    event.target.title.value = "";
+    event.target.body.value = "";
+    // event.target.assignee.value = "";
+    event.target.milestone.value = "";
+    event.target.labels.value = "";
+
+    return false;
   },
-  'click .get-repos': function () {
+  "change #repoSelect": function(evt) {
+    Session.set("repo_url", $(evt.target).val());
+    
     var token = Meteor.user().services.github.accessToken;
-    var user = Meteor.user().services.github.username;
     var options = {
       params: {
         "access_token": token
       }
     };
-    getRepos(options);
-  },
-  "change #repoSelect": function(evt) {
-    Session.set("repo_url", $(evt.target).val());
-    console.log(Session.get("repo_url"));
+    getIssues(Session.get("repo_url") + "/issues", options);
+    getAssignees();
   }
 });
