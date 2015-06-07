0e4ba7c1c25c2115137149b54a4e8201a6769737
Eric Lehner <ericj@buffalo.edu>
2015-06-06 20:26:33 -0400

    Added markdown for the issues

--- intellit.js
+++ intellit.js
@@ -1,3 +1,11 @@
+if(Meteor.isClient) {
+  Meteor.startup(function() {
+    $(window).resize(function(evt) {
+      Session.set("touch", new Date());
+    });
+  });
+}
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
